f0ce252852d51e4dd63fb58378100e9a490bd898
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 14:34:40 -0400

    Update HuluState.java

--- HuluState.java
+++ HuluState.java
@@ -3,6 +3,9 @@ public class HuluState implements State {
     private String[] movies = {"Run", "Nomadland", "Boss Level", "2067", "Girl"};
     private String[] shows = {"Eric Andre Show", "Letterkenny", "Snowfall", "Manifest", "Big"};
 
+    /*
+    load an instance of the TV into a Hulu state
+    */
     public HuluState(TV tv) {
         this.tv = tv;
     }
@@ -10,25 +13,25 @@ public class HuluState implements State {
     @Override
     public void pressHomeButton() {
         System.out.println("Loading Home Screen...");
-        tv.setState(tv.getHomeState());
+        tv.setState(tv.getHomeState());  // change the state of the TV to Home
     }
 
     @Override
     public void pressNetflixButton() {
         System.out.println("Loading Netflix...");
-        tv.setState(tv.getNetflixState());
+        tv.setState(tv.getNetflixState());  // change the state of the TV to Netflix
     }
 
     @Override
     public void pressHuluButton() {
-        System.out.println("We are already in Hulu.");
+        System.out.println("We are already in Hulu.");  // already in the Hulu state
     }
 
     @Override
     public void pressMovieButton() {
         System.out.println("**********Movies**********");
-        for (int i = 0; i < movies.length; i++) {
-            System.out.println(movies[i]);
+        for (int i = 0; i < movies.length; i++) {  // iterate over the size of the movie list
+            System.out.println(movies[i]);  // print the movie stored in that index
         }
         System.out.println("**************************");
         System.out.println();
@@ -37,8 +40,8 @@ public class HuluState implements State {
     @Override
     public void pressTVButton() {
         System.out.println("********TV Shows*********");
-        for (int i = 0; i < shows.length; i++) {
-            System.out.println(shows[i]);
+        for (int i = 0; i < shows.length; i++) {  // iterate over the size of the show list
+            System.out.println(shows[i]);  // print the show stored at that index
         }
         System.out.println("**************************");
         System.out.println();
