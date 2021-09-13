e1f8a3b70fce32a34841b0bd8f0a686970ea91c0
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 14:32:50 -0400

    Update HomeState.java

--- HomeState.java
+++ HomeState.java
@@ -1,35 +1,38 @@
+/*
+This class describes the TV when it is "on the home screen"
+*/
 
 public class HomeState implements State {
     private TV tv;
 
-    public HomeState(TV tv) {
+    public HomeState(TV tv) {  // establish a new instance of a TV
         this.tv = tv;
     }
 
     @Override
-    public void pressHomeButton() {
+    public void pressHomeButton() {  // simulating the home button being pressed
         System.out.println("TV is already on the home screen.");
     }
 
     @Override
     public void pressNetflixButton() {
         System.out.println("Loading Netflix...");
-        tv.setState(tv.getNetflixState());
+        tv.setState(tv.getNetflixState());  // change the state of the TV to Netflix
     }
 
     @Override
     public void pressHuluButton() {
         System.out.println("Loading Hulu...");
-        tv.setState(tv.getHuluState());
+        tv.setState(tv.getHuluState());  // change the state of the TV to Hulu
     }
 
     @Override
     public void pressMovieButton() {
-        System.out.println("Home: You must pick an app to show movies.");
+        System.out.println("Home: You must pick an app to show movies.");  // cannot show movies from home state
     }
 
     @Override
     public void pressTVButton() {
-        System.out.println("Home: You must pick an app to show tv shows.");
+        System.out.println("Home: You must pick an app to show tv shows.");  // cannot show TV shows from home state
     }
 }
